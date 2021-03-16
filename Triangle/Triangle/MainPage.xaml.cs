2118c6ebe523987e900afd98cac2cd826b49f9b7
Ильгар Шахбазов <ilgar.shahbazov@gmail.com>
2021-03-16 14:49:43 -0700

    Develop: create app without validators

---
+++ Triangle/Triangle/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Triangle
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonRunClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Answer", GetTriangleType(Int32.Parse(A.Text), Int32.Parse(B.Text), Int32.Parse(C.Text)), "Ok");
+        }
+
+        private string GetTriangleType(int a, int b, int c)
+        {
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                return "Triangle doesn't exist!";
+            }
+            else if (a != b && a != c && b != c)
+            {
+                return "It's Scalene Triangle!";
+            }
+            else if (a == b && b == c && c == a)
+            {
+                return "It's Equilateral Triangle!";
+            }
+            else
+            {
+                return "It's Isosceles Triangle!";
+            }
+
+
+
+        }
+    }
+}
